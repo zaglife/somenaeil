6498b93cceef01a0690af0322c215595b9536222
slea1 <slea1@naver.com>
2020-11-11 13:16:58 +0900

    class 작업완료
    class 작업완료

---
+++ somenaeil/src/com/vote/vote.java
@@ -0,0 +1,12 @@
+package com.vote;
+
+public class vote {
+	private int num number(4);
+	private String nick;
+	private String title;
+	private Calendar start_date;
+	private Calendar end_date;
+	private int multi_choice;
+	private int hidden_vote;
+	private int voting_stat;
+}
